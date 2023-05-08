36afb692894a16d533913d1ab118aa70ef7ad099
Sohel Rana <expartdesigns5@gmail.com>
2023-05-08 12:26:43 +0600

    complelet feature section

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import Container from './layouts/Container'
 import axios from 'axios'
+import Button from './layouts/Button'
 
 const Feature = () => {
     let [feature, setFeature] = useState({})
+    let [loading, setLoading]= useState(true)
     useEffect(()=>{
         async function features (){
             let featuredata = await axios.get("https://bwfc-api.vercel.app/feature")
             setFeature(featuredata.data)
+            setLoading(false)
+            console.log(feature.button.text);
         }
         features()
-        console.log(feature);
     },[])
+    if(loading){
+        return
+    }
   return (
-    <section className='pt-24' style={{backgroundImage: `url(${feature.featureShape})`}}>
+    <section className='p-24' style={{backgroundImage: `url(${feature.featureShape})`}}>
         <Container>
             <div className='flex'>
                 <div className='w-1/2'>
@@ -23,6 +29,8 @@ const Feature = () => {
                     <h3 className='font-man text-lg font-extrabold mb-6 text-primary capitalize'>{feature.subTitle}</h3>
                     <h2 className='font-man text-[50px] font-bold text-black mb-8'>{feature.title}</h2>
                     <p className='font-man text-lg font-normal mb-7 text-[#757095] w-[512px]'>{feature.paragraph}</p>
+                    <Button title={feature.button.text} />
+                    {/* <button>{feature.button.text}</button> */}
                 </div>
             </div>
         </Container>
---
+++ src/components/layouts/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({title}) => {
+  return (
+    <button className='py-4 px-10 bg-primary text-white border-solid rounded font-man text-base font-bold'>{title}</button>
+  )
+}
+
+export default Button
