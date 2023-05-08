312b889748951c96a76fce0b97dd39ce37842e10
Sohel Rana <expartdesigns5@gmail.com>
2023-05-08 23:31:14 +0600

    payment section complate

--- src/components/Ourfeature.js
+++ src/components/Ourfeature.js
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from './layouts/Container'
 import Haddingtop from './layouts/Haddingtop'
 import Haddinglow from './layouts/Haddinglow'
 import Para from './layouts/Para'
+import axios from 'axios'
+import { Form } from 'react-router-dom'
+import Input from './layouts/Input'
 
 
 const Ourfeature = () => {
+  let [payment, setpayment] = useState()
+  let [loadding, setloadding] = useState(true)
+  useEffect(()=>{
+    async function payments(){
+      let paymentdata = await axios.get("https://bwfc-api.vercel.app/payment")
+      setpayment(paymentdata.data)
+      setloadding(false)
+      console.log(payment);
+    }
+    payments()
+  },[])
+  if(loadding){
+    return
+  }
   return (
     <Container>
-        <div className='flex justify-between py-20 bg-[#F3F7FA]'>
-            <div className='ml-16'>
-                <Haddinglow title='GGG'/>
-                <Haddingtop title='hhhhh'/>
-                <Para title='ssssss'/>
+        <div className='flex py-20 bg-[#F3F7FA]'>
+            <div className='ml-16 w-[60%]'>
+                <Haddinglow title={payment.subTitle}/>
+                <Haddingtop title={payment.title}/>
+                <Para title={payment.paragraph}/>
             </div>
-            <div className='mr-28'>2</div>
+            <div className='mr-28 w-[40%] relative'>
+                <h3 className='font-man font-bold text-2xl text-primary mb-6'>Get Started for Free</h3>
+                <Form>
+                  <Input  placeholder='Email Address'/>
+                  <Input  placeholder='Password'/>
+                  <button className='bg-[#FF7F5C] py-4 px-[135px] font-man font-bold text-base text-white'>Get Started</button>
+                </Form>
+              <img className='absolute -top-28 -right-28 w-[150px]' src={payment.paymentShape}/>
+            </div>
         </div>
     </Container>
   )
---
+++ src/components/layouts/Input.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Input = ({placeholder}) => {
+  return (
+    <input className='w-[346px] pt-[18px] pb-[12px] pl-[21px] rounded font-man font-normal text-xl mb-6' placeholder={placeholder} />
+  )
+}
+
+export default Input
