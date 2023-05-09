43ccebb780b870a6b44a85ecf371fac8f2ecef1d
Sohel Rana <expartdesigns5@gmail.com>
2023-05-09 10:37:18 +0600

    slider

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Container from './layouts/Container'
 import axios from 'axios'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Client = () => {
     let [client, setclient] = useState()
@@ -17,6 +20,13 @@ const Client = () => {
     if(lodding){
         return
     }
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
   return (
     <Container>
         <div className='mb-10'>
@@ -25,7 +35,27 @@ const Client = () => {
             <h2 className='font-man font-extrabold text-[40px] w-[350px]'>{client.title}</h2>
         </div>
         <div>
-            
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
         </div>
         <div>2</div>
     </Container>
